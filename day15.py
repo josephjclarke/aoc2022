367c5bc24f3727db9d01825be0b0ebeaa2722876
Joe Clarke <granmaestrojoe@gmail.com>
2022-12-15 23:51:21 +0000

    now it runs at a reasonable pace

--- day15.py
+++ day15.py
@@ -1,31 +1,18 @@
-import numpy as np
-
+critical_y = 2_000_000
+bound      = 4_000_000
 
 def l1(a, b):
     return int(abs(a.real - b.real) + abs(a.imag - b.imag))
 
-
-def nearestSensor(point, sensors):
-    m = float("inf")
-    for idx in range(len(sensors)):
-        n = l1(point, sensors[idx])
-        if n < m:
-            ret = idx
-            m = n
-    return ret
-
+def cmplx(a):
+    return a[0]+1j*a[1]
 
 sensors, beacons = zip(
-    *[(list(map(lambda x: int(x.split("=")[1]), l[10:].split(","))),
-       list(map(lambda x: int(x.split("=")[1]), r[22:].split(","))))
+    *[(cmplx(list(map(lambda x: int(x.split("=")[1]), l[10:].split(",")))),
+       cmplx(list(map(lambda x: int(x.split("=")[1]), r[22:].split(",")))))
       for l, r in
       [line.rstrip().split(":") for line in open("input15.txt").readlines()]])
 
-critical_y = 2000000
-bound = 4000000
-
-sensors = [s[0] + 1j * s[1] for s in sensors]
-beacons = [b[0] + 1j * b[1] for b in beacons]
 
 nobeacons = set()
 for s, b in zip(sensors, beacons):
@@ -39,19 +26,22 @@ for s, b in zip(sensors, beacons):
 nobeacons -= {int(b.real) for b in beacons if int(b.imag) == critical_y}
 print(len(nobeacons))
 
-idx = list(range(len(sensors)))
-ds = [-l1(b, s) for s, b in zip(sensors, beacons)]
-idx.sort(key=ds.__getitem__)
-sensors = list(map(sensors.__getitem__, idx))
-beacons = list(map(beacons.__getitem__, idx))
-for x in range(0, bound):
-    for y in range(0, bound):
-        p = x + 1j * y
-        not_excluded = True
-        for s, b in zip(sensors, beacons):
-            if l1(s, p) <= l1(s, b):
-                not_excluded = False
-                break
-        if not_excluded:
-            print(int(bound * p.real + p.imag))
-            raise SystemExit
+def test(z):
+    if int(z.real) not in range(0,bound) or int(z.imag) not in range(0,bound):
+        return False
+    for s,b in zip(sensors,beacons):
+        if l1(z,s) <= l1(s,b):
+            return False
+    return True
+
+def onlypoint():
+    for s,b in zip(sensors,beacons):
+        d = l1(s,b)
+        #must fall just outside the boundary of a scanned zone
+        circle = [s + x + 1j*(d+1 - x) for x in range(-d-1,d+2)] + [s + x -1j*(d+1-x) for x in range(-d-1,d+2)]
+        for p in circle:
+            if test(p):
+                return p
+
+p = onlypoint()
+print(int(p.real * 4000000 + p.imag)) 
