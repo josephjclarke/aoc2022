4bd4b3544b3f0d99c7a592fd61f3b952fab2d68a
Joe Clarke <j.j.clarke@exeter.ac.uk>
2022-12-15 09:53:24 +0000

    started parsing day 15

---
+++ day15.py
@@ -0,0 +1,5 @@
+lines = [line.rstrip().split(":") for line in open("input15.txt").readlines()]
+lines2 = [(list(map(lambda x: int(x.split("=")[1]), l[10:].split(","))),
+           list(map(lambda x: int(x.split("=")[1]), r[22:].split(","))))
+          for l, r in lines]
+print(lines2)
