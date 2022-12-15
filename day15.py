9235d38efeed5566166cfbc825d1efcc60229afc
Joe Clarke <granmaestrojoe@gmail.com>
2022-12-15 11:10:35 +0000

    not working yet...

--- day15.py
+++ day15.py
@@ -1,6 +1,18 @@
-lines = [line.rstrip().split(":") for line in open("input15.txt").readlines()]
-lines2 = [(list(map(lambda x: int(x.split("=")[1]), l[10:].split(","))),
+def l1(a,b):
+    return int(abs(a.real-b.real) + abs(b.imag - b.imag))
+
+sensors,beacons = zip(*[(list(map(lambda x: int(x.split("=")[1]), l[10:].split(","))),
            list(map(lambda x: int(x.split("=")[1]), r[22:].split(","))))
-          for l, r in lines]
-sensors, beacons = zip(*lines2)
-print(sensors, beacons)
+          for l, r in [line.rstrip().split(":") for line in open("input15.test").readlines()]])
+
+critical_y = 10#2000000
+sensors = [s[0] + 1j *s[1] for s in sensors]
+beacons = [b[0] + 1j *b[1] for b in beacons]
+
+
+nobeacons = set()
+for s,b in zip(sensors,beacons):
+    maxdist = l1(s,b)
+    nobeacons.update([x for x in range(int(s.real)-maxdist,int(s.real)+maxdist) if l1(s,x + critical_y *1j) <= maxdist])
+#remove beacons
+print(len(nobeacons - {b.real for b in beacons if b.imag==critical_y}))
